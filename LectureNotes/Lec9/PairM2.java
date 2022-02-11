6bec5f73a12bcefc9980dc21f7391f2092b444d6
thePianoKid <gbraden.dev@gmail.com>
2022-02-11 14:58:59 -0500

    updating lecture 9 notes

---
+++ LectureNotes/Lec9/PairM2.java
@@ -0,0 +1,17 @@
+public class PairM2<X, Y> {
+    private X first;
+    private Y second;
+
+    public PairM2(X first, Y second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public X getFirst() {
+        return first;
+    }
+
+    public Y getSecond() {
+        return second;
+    }
+}
--- LectureNotes/Lec9/Test.java
+++ LectureNotes/Lec9/Test.java
@@ -1,12 +1,27 @@
 public class Test {
     public static void main(String[] args) {
+        // Testing Pair.java
+
         Account a, b;
 
         a = new Account(1, "Yo");
         b = new Account(1, null);
 
         Pair<Account> p = new Pair<Account>(a, b);
+
+        Pair<String> p2 = new Pair<String>("Donald", "Trump");
         
+        Pair<Integer> p3 = new Pair<Integer>(1, 2);
+
         System.out.println("p.getName(): " + p.getFirst().getName());
+        System.out.println(p2.getSecond());
+        System.out.println(p3.getSecond());
+        
+        // Test PairM2.java
+        
+        PairM2<Integer, String> p4 = new PairM2<Integer, String>(1, "asdf");
+        
+        Integer i = p4.getFirst();
+        String j = p4.getSecond();
     }
 }
