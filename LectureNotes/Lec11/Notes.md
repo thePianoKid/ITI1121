eccf836cb1adb72c3dce12051743cc651e54d01f
thePianoKid <gbraden.dev@gmail.com>
2022-02-16 15:41:00 -0500

    Lecture 11 notes

---
+++ LectureNotes/Lec11/Notes.md
@@ -0,0 +1,9 @@
+# Lecture 11
+> See Lecture 10 for the implementation of array stacks
+
+Stack-based algorithms are used for *syntactical analysis* (parsing)
+
+Example: `1 + 2 * 3 - 4`
+
+Each operation is put into the stack and computed individually
+
---
+++ LectureNotes/Lec11/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void scan(String expression) {
+        Reader reader = new Reader(expression);
+
+        while (reader.hasMoreTokens()) {
+            System.out.println(reader.nextToken());
+        }
+    }
+
+    public static void main (String[] args) {
+        Test.scan("3     + 6    *    90");
+    }
+}
