47bd09493d3cf714f00850fa379f0cdcf83628b4
thePianoKid <gbraden.dev@gmail.com>
2022-03-29 16:25:25 -0400

    completed suggested exercises for lab 9

---
+++ Labs/lab9_300154707/Ex/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        try {
+            Find.find("Ottawa.txt", "Ottawa");
+        } catch (Exception e) {
+
+        } finally {
+
+        }
+    }
+}
