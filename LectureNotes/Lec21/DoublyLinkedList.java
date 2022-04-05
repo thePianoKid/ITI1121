1bad9d065433af470608619c3550c14afc0c9191
thePianoKid <gbraden.dev@gmail.com>
2022-04-05 14:09:27 -0400

    copied DoublyLinkedList from class github repo

--- LectureNotes/Lec21/DoublyLinkedList.java
+++ LectureNotes/Lec21/DoublyLinkedList.java
@@ -1,296 +1,216 @@
-public class DoublyLinkedList<E> implements List<E>{
-    private static class Node<T> {
-        private T value;
-        private Node<T> previous;
-        private Node<T> next;
-        private Node(T value, Node<T> previous, Node<T> next) {
-            this.value = value;
-            this.previous = previous;
-            this.next = next;
+
+public class DoublyLinkedList<E> implements List<E> {
+
+	private static class Node<T> {
+
+		private T value;
+		private Node<T> next;
+		private Node<T> previous;
+
+		private Node(T value, Node<T> previous, Node<T> next ){
+			this.value = value;
+			this.next = next;
+			this.previous = previous;
+		}
+	}
+
+	private Node<E> head;
+	private int size = 0 ;
+
+	public DoublyLinkedList(){
+		size = 0;
+		head = new Node<E>(null,null,null);
+		head.next = head.previous = head;	// HERE
+	}
+
+
+	public int size(){
+		return size;
+	}
+
+	public boolean isEmpty(){
+		return size == 0;
+	}
+
+	public void addFirst(E o){
+
+        if(o == null) {
+            throw new NullPointerException("Can't add null reference to the list");
         }
-    }
 
-    private Node<E> head;
-    private Node<E> tail;
+        // HERE
 
-    public DoublyLinkedList() {
-        head = null;
-        tail = null;
-    }
+		Node<E> newNode = new Node<E>(o, head, head.next);	
+		head.next = newNode;
+		newNode.next.previous = newNode;
+		size++;
+	}
 
-    public int size() {
-        int size = 0;
-        Node<E> p = head;
+	public void add(E o){
+
+        if(o == null) {
+            throw new NullPointerException("Can't add null reference to the list");
+        }
+
+        // HERE
+
+		Node<E> newNode = new Node<E>(o, head.previous, head);
+		head.previous = newNode;
+		newNode.previous.next = newNode;
+		size++;
+
+	}
+
+    public void add( int pos, E o) {
+
+        if(o == null) {
+            throw new NullPointerException("Can't add null reference to the list");
+        }
+        if ( pos < 0 || pos > size) {
+            throw new IndexOutOfBoundsException( Integer.toString( pos ) );
+        }
+
+        // HERE
+
+	    Node<E> p = head;
+	    
+	    for (int i = 0; i < pos; i++) {
+		    p = p.next;
+		}
+	    
+	    p.next = new Node<E> (o, p, p.next);
+		p.next.next.previous = p.next;
+		size++;
         
-        while (p != null) {
-            p = p.next;
-            size++;
-        }
-
-        return size;
     }
-
-    /**
-     * Adds o to the front of the list
-     * @param o
-     */
-    public void addFirst(E o) {
-        if (o == null) {
-            throw new NullPointerException("Can't add null reference to the list.");
-        }
-        head = new Node<E>(o, null, head);
-    }
-    /**
-     * Adds o to the back of the list
-     * @param o
-     */
-    public void add(E o) {
-        if (o == null) {
-            throw new NullPointerException("Can't add null reference to the list.");
-        }
-
-        Node<E> newNode = new Node<E>(o, null, null);
-        
-        if (head == null) { // special case
-            head = newNode;
-        } else { // general case
-            Node<E> p = head;
-
-            while (p.next != null) {
-                p = p.next;
-            }
-
-            newNode.previous = p;
-            p.next = newNode;
-        }
-    }
-
-    public void add(int pos, E o) {
-        if (o == null) {
-            throw new NullPointerException("No null in maa haus!");
-        }
-
-        if (pos < pos || pos > size()) {
-            throw new IndexOutOfBoundsException();
-        }
-
-        if (pos == 0) { // special case: adding at position 0
-            head = new Node<E>(o, null, head);
-            if (tail == null) { // special case: list was empty
-                tail = head;
-            } else {
-                head.next.previous = head;
-            }
-        } else {
-            Node<E> p = head;
-
-            for (int i = 0; i < pos-1; i++) {
-                if (p == null) {
-                    throw new IndexOutOfBoundsException(Integer.toString(pos));
-                } else {
-                    p = p.next;
-                }
-            }
-
-            Node<E> q = p.next;
-            p.next = new Node<E>(o, p, q);
-            if (p == tail) {
-                tail = p.next;
-            } else {
-                q.previous = p.next;
-            }
-        }
-    }
-
-    public void addLast(E t) {
-        Node<E> newNode = new Node<E>(t, null, null);
-        if (head == null) {
-            head = tail = newNode;
-        } else {
-            newNode.previous = tail;
-            tail.next = newNode;
-            tail = tail.next;
-        }
-    }
-
-    public E removeFirst() {
-        if (isEmpty()) {
-            throw new IllegalStateException("List is empty.");
-        }
-
-        E savedValue = head.value;
-        head = head.next;
-        
-        // if the head is null, the list is empty, so the tail should be wiped of its value
-        if (head == null) {
-            tail = null;
-        }
-
-        return savedValue;
-    }
-
-    public E removeLast() {
-        E result;
-
-        if (isEmpty()) {
-            throw new IllegalStateException();
-        } else if (head.next == null) { // special case
-            result = head.value;
-            head = null;
-        } else { // general case
-            Node<E> p = head;
-
-            while(p.next.next != null) {
-                p = p.next;
-            }
-
-            result = p.next.value;
-            p.next = null;
-        }
-
-        return result;
-    }
-
-    public boolean remove(E o) {
-        // If the list is empty, return false
-        if (head == null) {
-             return false;
-        }
-
-        Node<E> toDelete = null;
-        // If the first element equals o, make the second element the new first element
-        if (head.value.equals(o)) {
-            toDelete = head;
-            head = head.next;
-        // General case
-        } else {
-            Node<E> p = head;
-
-            // Look through the linked list and exit the loop only when the element is found, 
-            // or when the end of the loop has been reached
-            while (p.next != null && !p.next.value.equals(o)) {
-                p = p.next;
-            }
-            
-            // If the end of the loop was reached, return false
-            if (p.next == null) {
-                return false;
-            }
-
-            // If the element was found, replace it with the next element in the list
-            toDelete = p.next;
-            p.next = toDelete.next;
-        }
-
-        // scrubby scrubby...
-        toDelete.value = null;
-        toDelete.next = null;
-
-        return true; 
-    }
-
-    public E remove(int pos) {
-        // TODO: size() method is ineffcient; implement an instance variable
-        if (pos < 0) {
-            throw new IndexOutOfBoundsException("Index cannot be negative.");
-        }
-
-        if (isEmpty()) {
-            throw new IllegalStateException("The list is empty.");
-        }
-
-        Node<E> toRemove;
-        if (pos == 0) {
-            toRemove = head;
-            head = head.next;
-        } else {            
-            Node<E> p = head;
     
-            for (int i = 0; i < pos-1; i++) {
-                if (p == null) {
-                    throw new IndexOutOfBoundsException("Index is greater than the list.");
-                }
-                p = p.next;
-            }
+    
+	public E removeFirst() {
+		if(isEmpty())
+			throw new IllegalStateException("Empty list!");
 
-            toRemove = p.next;
-            if (p.next != null) {
-                p.next = p.next.next;
-            }
+		// HERE
+
+		E savedValue = head.next.value;
+		head.next = head.next.next;
+		head.next.previous = head;
+		size--;
+		return savedValue;
+	}
+
+	public E removeLast(){
+		if(isEmpty())
+			throw new IllegalStateException("Empty list!");
+
+		// HERE
+
+		E savedValue = head.previous.value;
+		head.previous = head.previous.previous;
+		head.previous.next = head;
+		size--;
+		return savedValue;
+
+	}	
+
+	public boolean remove(E o) {
+		if (isEmpty()) return false;
+
+		// HERE
+
+		Node<E> p = head.next;
+		while((p != head) && !(p.value.equals(o))) {
+				p = p.next;
+		}
+		if(p == head)
+			return false;
+		
+		p.next.previous = p.previous;
+		p.previous.next = p.next;
+		size --;
+		return true;
+
+	}
+   
+    public E get( int pos ) {
+
+        if(isEmpty())
+            throw new IllegalStateException("Empty list!");
+
+        if ( pos < 0 || pos >= size ) {
+            throw new IndexOutOfBoundsException( Integer.toString( pos ) );
         }
 
-        E savedValue = toRemove.value;
-        toRemove.value = null;
-        toRemove.next = null;
+        // HERE
 
-        return savedValue;
-    }
-
-    public E get(int pos) {
-        // TODO: size() method is ineffcient; implement an instance variable
-        if (pos < 0) {
-            throw new IndexOutOfBoundsException("Index cannot be negative.");
-        }
-
-        if (isEmpty()) {
-            throw new IllegalStateException("The list is empty.");
-        }
-
-        Node<E> p = head;
-
-        for (int i = 0; i<pos-1; i++) {
-            if (p == null) {
-                throw new IndexOutOfBoundsException("Index is larger than the list.");
-            }
+        Node<E> p = head.next;
+        
+        for ( int i=0; i<pos; i++ ) {
             p = p.next;
         }
 
         return p.value;
     }
 
-    public boolean isEmpty() {
-        return head == null;
-    }
+   	public E remove( int pos ) {
 
-    public boolean equals(DoublyLinkedList<E> otherList) {
-        // This is the standard algo for checking for equality in list-like classes
-        int size = this.size();
-        if (otherList == null || size != otherList.size()) {
-            return false;
-        } 
+        if(isEmpty())
+            throw new IllegalStateException("Empty list!");
 
-        Node<E> thisCursor = this.head;
-        Node<E> otherCursor = otherList.head;
-        
-        for (int i = 0; i < size; i++) {
-            if (!thisCursor.value.equals(otherCursor.value)) {
-                return false;
-            }
-            
-            thisCursor = thisCursor.next;
-            otherCursor = otherCursor.next;
-        }
-        
-        return true;
-    }
-
-    public String toString() {
-        StringBuffer buffer = new StringBuffer("[");
-        
-        if (!isEmpty()) {
-            // This case is separated because we don't want a comma at the start 
-            // of the front element
-            Node<E> cursor = head;
-            buffer.append(cursor.value);
-
-            while (cursor.next != null) {
-                cursor = cursor.next;
-                buffer.append("," + cursor.value);
-            }
+        if ( pos < 0 || pos >= size ) {
+            throw new IndexOutOfBoundsException( Integer.toString( pos ) );
         }
 
-        buffer.append("]");
+        // HERE
 
-        return buffer.toString();
+        Node<E> p = head.next;
+        
+        for ( int i=0; i<pos; i++ ) {
+            p = p.next;
+        }
+
+		p.next.previous = p.previous;
+		p.previous.next = p.next;
+		size --;
+		return p.value;
+    }
+
+  	public boolean equals(DoublyLinkedList<E> otherList){
+
+    	if(otherList == null)
+    		return false;
+
+    	if(size != otherList.size())
+    		return false;
+
+    	// HERE
+
+    	Node<E> thisCursor = head.next;
+    	Node<E> otherCursor = otherList.head.next;
+
+    	for(int i = 0; i < size; i++){
+    		if(!thisCursor.value.equals(otherCursor.value))
+    			return false;
+    		thisCursor = thisCursor.next;
+    		otherCursor = otherCursor.next;
+    	}
+
+    	return true;
+    }
+
+
+    public String toString(){
+    	StringBuffer res = new StringBuffer();
+    	res.append("[");
+    	if(!isEmpty()){
+    		// HERE
+ 	 	 	Node<E> cursor = head.next;
+    		res.append(cursor.value);
+    		while(cursor.next != head){
+    			cursor = cursor.next;
+	    		res.append(" " + cursor.value);
+    		}
+    	}
+    	res.append("]");
+    	return res.toString();
     }
 }
